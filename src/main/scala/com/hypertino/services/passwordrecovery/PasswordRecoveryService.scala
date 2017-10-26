77ab604fefadd8ffb411a906350815b3d949eb29
Maga Abdurakhmanov <maqdev@gmail.com>
2017-10-27 01:11:44 +0200

    + initial version

--- src/main/scala/com/hypertino/services/passwordrecovery/PasswordRecoveryService.scala
+++ src/main/scala/com/hypertino/services/passwordrecovery/PasswordRecoveryService.scala
@@ -8,12 +8,18 @@
 
 package com.hypertino.services.passwordrecovery
 
-import com.hypertino.binders.value.{Null, Value}
+import java.net.URLEncoder
+import java.util.Base64
+
+import com.hypertino.binders.value.{Lst, Null, Obj, Value}
 import com.hypertino.hyperbus.Hyperbus
-import com.hypertino.hyperbus.model.{Created, DynamicBody}
+import com.hypertino.hyperbus.model.{Accepted, BadRequest, Conflict, Created, DynamicBody, EmptyBody, ErrorBody, Ok, ResponseBase, Unauthorized}
 import com.hypertino.hyperbus.subscribe.Subscribable
 import com.hypertino.service.control.api.Service
 import com.hypertino.services.passwordrecovery.api.PasswordRecoveriesPost
+import com.hypertino.services.passwordrecovery.apiref.authpin.{CreatePin, PinsPost}
+import com.hypertino.services.passwordrecovery.apiref.email.{EmailMessage, EmailsPost}
+import com.hypertino.services.passwordrecovery.apiref.user.UsersGet
 import com.typesafe.config.Config
 import com.typesafe.scalalogging.StrictLogging
 import monix.eval.Task
@@ -22,6 +28,7 @@ import scaldi.{Injectable, Injector}
 
 import scala.concurrent._
 import scala.concurrent.duration.FiniteDuration
+import scala.util.Success
 
 case class PasswordRecoveryServiceConfig(
                               smtpHost: String,
@@ -44,7 +51,40 @@ class PasswordRecoveryService(implicit val injector: Injector) extends Service w
 
   private val handlers = hyperbus.subscribe(this, logger)
 
-  def onEmailsPost(implicit p: PasswordRecoveriesPost): Task[Created[DynamicBody]] = ???
+  def onEmailsPost(implicit p: PasswordRecoveriesPost): Task[Accepted[DynamicBody]] = {
+    p.body.email.map { email ⇒
+      hyperbus
+        .ask(UsersGet(query = Obj.from("email" → email)))
+        .flatMap {
+          case Ok(DynamicBody(None,Lst(items)), _) ⇒ {
+            if (items.isEmpty || items.tail.nonEmpty) {
+              Task.raiseError(Conflict(ErrorBody("user-not-found", Some(s"User with '$email' is not found"))))
+            }
+            else {
+              val user = items.head
+              val user_id = user.dynamic.user_id.toString
+              val pinId = "pwd-" + user_id
+
+              hyperbus
+                .ask(PinsPost(CreatePin(pinId, identityKeys=Obj.from("user_id" → user_id, "email" → email))))
+                .flatMap { r ⇒
+                  val pin = r.body.pin
+                  val pin64 = new String(Base64.getEncoder.encode((pinId + ":" + pin).getBytes("UTF-8")), "UTF-8")
+                  val emailData = Obj.from("user" → user, "pin" → pin64)
+                  hyperbus
+                    .ask(EmailsPost(EmailMessage("recovery-password",p.body.language,emailData)))
+                    .map { _ ⇒
+                      Accepted(EmptyBody)
+                    }
+                }
+            }
+          }
+        }
+    } getOrElse {
+      // email is optional, in future there could be different recovery options (phone?)
+      Task.raiseError(BadRequest(ErrorBody("email-is-not-specified")))
+    }
+  }
 
   override def stopService(controlBreak: Boolean, timeout: FiniteDuration): Future[Unit] = Future {
     handlers.foreach(_.cancel())
--- src/test/scala/com/hypertino/services/passwordrecovery/PasswordRecoveryServiceSpec.scala
+++ src/test/scala/com/hypertino/services/passwordrecovery/PasswordRecoveryServiceSpec.scala
@@ -8,24 +8,28 @@
 
 package com.hypertino.services.passwordrecovery
 
-import javax.mail.internet.InternetAddress
+import java.util.Base64
 
-import com.hypertino.binders.value.Obj
+import com.hypertino.binders.value.{Lst, Null, Obj}
 import com.hypertino.hyperbus.Hyperbus
-import com.hypertino.hyperbus.model.MessagingContext
+import com.hypertino.hyperbus.model.{Accepted, Conflict, Created, DynamicBody, EmptyBody, MessagingContext, Ok}
 import com.hypertino.hyperbus.subscribe.Subscribable
 import com.hypertino.hyperbus.transport.api.ServiceRegistrator
 import com.hypertino.hyperbus.transport.registrators.DummyRegistrator
 import com.hypertino.service.config.ConfigLoader
+import com.hypertino.services.passwordrecovery.api.{InitRecovery, PasswordRecoveriesPost}
+import com.hypertino.services.passwordrecovery.apiref.authpin.{NewPin, PinsPost}
 import com.hypertino.services.passwordrecovery.apiref.email.EmailsPost
+import com.hypertino.services.passwordrecovery.apiref.user.UsersGet
 import com.typesafe.config.Config
+import monix.eval.Task
 import monix.execution.Scheduler
-import org.jvnet.mock_javamail.Mailbox
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.time.{Seconds, Span}
 import org.scalatest.{BeforeAndAfterAll, FlatSpec, Matchers}
 import scaldi.Module
 
+import scala.collection.mutable
 import scala.concurrent.duration._
 
 class PasswordRecoveryServiceSpec extends FlatSpec with Module with BeforeAndAfterAll with ScalaFutures with Matchers with Subscribable {
@@ -38,16 +42,60 @@ class PasswordRecoveryServiceSpec extends FlatSpec with Module with BeforeAndAft
   bind [ServiceRegistrator] to DummyRegistrator
 
   val hyperbus = inject[Hyperbus]
+  hyperbus.subscribe(this)
   Thread.sleep(500)
 
   val service = new PasswordRecoveryService()
+  val emails = mutable.ArrayBuffer[EmailsPost]()
 
   override def afterAll() {
     service.stopService(false, 10.seconds).futureValue
     hyperbus.shutdown(10.seconds).runAsync.futureValue
   }
 
+  def onUsersGet(implicit r: UsersGet) = Task.eval { Ok(DynamicBody(
+      if (r.headers.hrl.query.dynamic.email.toString == "john@example.net") {
+        Lst.from(Obj.from("user_id" → "100500", "first_name" → "John", "last_name" → "Malkovich", "email" → "john@example.net"))
+      }
+      else {
+        Lst.empty
+      }
+    ))
+  }
+
+  def onPinsPost(implicit r: PinsPost) = Task.eval(Created(NewPin("123", r.body.pinId)))
+
+  def onEmailsPost(implicit r: EmailsPost) = Task.eval {
+    emails += r
+    Accepted(EmptyBody)
+  }
+
   "PasswordRecoveryService" should "create pin code and send email" in {
-    ???
+    val r = hyperbus
+      .ask(PasswordRecoveriesPost(InitRecovery(email=Some("john@example.net"))))
+      .runAsync
+      .futureValue
+
+    r shouldBe a[Accepted[_]]
+    emails.size shouldBe 1
+    val email = emails.head
+    emails.clear
+
+    email.body.template shouldBe "recovery-password"
+    val pin = email.body.data.dynamic.pin.toString
+    val s = new String(Base64.getDecoder.decode(pin.getBytes("UTF-8")), "UTF-8")
+    s should startWith("pwd-100500:")
+
+    email.body.data.dynamic.user.dynamic.email.toString shouldBe "john@example.net"
+  }
+
+  it should "conflict if no such user is found" in {
+    val r = hyperbus
+      .ask(PasswordRecoveriesPost(InitRecovery(email=Some("me@example.net"))))
+      .runAsync
+      .failed
+      .futureValue
+
+    r shouldBe a[Conflict[_]]
   }
 }
